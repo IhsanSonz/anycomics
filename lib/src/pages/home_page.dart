45aa6d6b637314e84b7ccb106060d70074c72ec1
IhsanSonz <mnurihsanmutaqin66@gmail.com>
2025-05-10 00:24:32 +0700

    feat: add gridview on homepage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,66 @@
 part of 'pages.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final List<String> items = List.generate(20, (index) => 'Item $index');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen Content', style: TextStyle(fontSize: 24)),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75,
+        ),
+        itemCount: items.length,
+        itemBuilder:
+            (BuildContext context, int index) => InkWell(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Tapped on Item $index')),
+                );
+              },
+              borderRadius: BorderRadius.circular(12),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                clipBehavior: Clip.antiAlias,
+                elevation: 4,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.asset(
+                      'assets/images/nano_machine.jpeg',
+                      fit: BoxFit.cover,
+                    ),
+                    Container(color: Color.fromRGBO(0, 0, 0, 0.4)),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Item $index',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 4,
+                                color: Colors.black45,
+                                offset: Offset(1, 1),
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
       ),
     );
   }
